f85af0056a4f0ea40faecdff1007f604389f3bac
WilliamTsai1227 <160149504+WilliamTsai1227@users.noreply.github.com>
2024-03-07 18:15:55 +0800

    Add files via upload

--- Tic_Tac_Toe_Game.py
+++ Tic_Tac_Toe_Game.py
@@ -1,132 +1,128 @@
-counter = 0
-row1 = [' ', ' ', ' ']
-row2 = [' ', ' ', ' ']  # 4, 5, 6 => row2 index 0, 1, 2
-row3 = [' ', ' ', ' ']  # 7, 8, 9
-prompt= "Player1:O ; Player2:X"
+# Display the grid            #做一個可以顯示九宮格的function
+# Accpet user input           #做一個user input function
+                              #做一個會不斷變動O X的function
+# Update the game grid        #做一個更新九宮格的function
+# Game win-checking algorithm #做一個輸贏確認的function
+# Improving game mechanism    #改進程式
+row1 = [' ',' ',' '] #user_choice 1 2 3 # index 0,1,2
+row2 = [' ',' ',' '] #user_choice 4 5 6 # index 0,1,2   # 4%3=1 ,5%3=2 ,6%3=0 #position(-1) 0 ,1 ,-1
+row3 = [' ',' ',' '] #user_choice 7 8 9 # index 0,1,2
+prompt = "User1:O ; User2:X"
+counter = -1
 
-
-def display(row1, row2, row3):
+def display():
+    print(prompt)
     print(row1)
     print(row2)
     print(row3)
 
-
 def user_choice():
     choice = input("Please enter a number (1-9): ")
-    while not choice.isdigit() or (int(choice) not in range(1, 10)):
+    while not choice.isdigit() or (int(choice) not in range(1,10)):
         if not choice.isdigit():
-            print("Sorry, your choice is not valid")
+            print("Input is invalid")
         else:
-            print("Your choice is not within the range of 1 - 9.")
+            print("Your choice is not within the range of 1-9")
         choice = input("Please enter a number (1-9): ")
     return int(choice)
 
-
-def getCurrentSymbol():
+def get_currentSymbol():
     global counter
-    symbol_list = ['X', 'O']
+    symbol_list = ["O","X"]
     counter += 1
-    return symbol_list[counter % 2]
-
-
-def update_table(index):
-    global row1, row2, row3
-    if index in range(1, 4):
-        if row1[index - 1] == ' ':
-            row1[index - 1] = getCurrentSymbol()
+    return symbol_list[counter%2]
+ 
+def update_table():
+    index = user_choice()
+    if index in range(1,4):
+        if row1[index-1] == " ":
+            row1[index-1] = get_currentSymbol()
             return True
         else:
             return False
-    elif index in range(4, 7):
-        if row2[index % 3 - 1] == ' ':
-            row2[index % 3 - 1] = getCurrentSymbol()
+    elif index in range(4,7):
+        if row2[index%3 -1] == " ":
+            row2[index%3 -1] = get_currentSymbol()
             return True
         else:
             return False
-    else:
-        if row3[index % 3 - 1] == ' ':
-            row3[index % 3 - 1] = getCurrentSymbol()
+    elif index in range(7,10):
+        if row3[index%3 -1] == " ":
+            row3[index%3 -1] = get_currentSymbol()
             return True
         else:
             return False
+        
+def win_or_lose():
+    empty_set = {" "}
+    setRow1 = set(row1)
+    setRow2 = set(row2)
+    setRow3 = set(row3)
+    decision = False
+    if empty_set.issubset(setRow1) == False:
+        if row1[0]==row1[1] and row1[1]==row1[2]:
+            display()
+            if row1[0]=="O":
+                print("User_1 win,congratulate!")
+            else:
+                print("User_2 win,congratulate!")
+            return True
+    if empty_set.issubset(setRow2) == False:
+        if row2[0]==row2[1] and row2[1]==row2[2]:
+            display()
+            if row2[0]=="O":
+                print("User_1 win,congratulate!")
+            else:
+                print("User_2 win,congratulate!")
+            return True
+    if empty_set.issubset(setRow3) == False:
+        if row3[0]==row3[1] and row3[1]==row3[2]:
+            display()
+            if row3[0]=="O":
+                print("User_1 win,congratulate!")
+            else:
+                print("User_2 win,congratulate!")
+            return True
+    for i in range(3):
+        if row1[i] != " " and row2[i] != " " and row3[i] != " ":   
+            if row1[i] == row2[i] and row2[i] == row3[i]:
+                display()
+                if row1[i] == "O":
+                        print("User_1 win,congratulate!")
+                else:
+                    print("User_2 win,congratulate!")
+                return True
+    if row1[0] != " " and row2[1] != " " and row3[2] != " ":   
+        if row1[0] == row2[1] and row2[1] == row3[2]:
+            display()
+            if row1[0] == "O":
+                print("User_1 win,congratulate!")
+            else:
+                print("User_2 win,congratulate!")
+            return True
+    if row1[2] != " " and row2[1] != " " and row3[0] != " ":   
+        if row1[2] == row2[1] and row2[1] == row3[0]:
+            display()
+            if row1[2] == "O":
+                print("User_1 win,congratulate!")
+            else:
+                print("User_2 win,congratulate!")
+            decision = True
+            return True
+    if counter == 8 :
+        print("Tie game.")
+        display()
+        return True                    
 
-
-def check_winning():
-    player_1_wins = False
-    player_2_wins = False
-    if (row1[0] == row1[1] and row1[1] == row1[2] and (" " not in row1)):
-        if (row1[0] == "X"):
-            player_2_wins = True
-        else:
-            player_1_wins = True
-    elif (row2[0] == row2[1] and row2[1] == row2[2] and (" " not in row2)):
-        if (row2[0] == "X"):
-            player_2_wins = True
-        elif (row2[0] == "O"):
-            player_1_wins = True
-    elif (row3[0] == row3[1] and row3[1] == row3[2] and (" " not in row3)):
-        if (row3[0] == "X"):
-            player_2_wins = True
-        elif (row3[0] == "O"):
-            player_1_wins = True
-    elif (row1[0] == row2[0] and row2[0] == row3[0] and (row1[0] != " " and row2[0] != " " and row3[0] != " ")):
-        if (row1[0] == "X"):
-            player_2_wins = True
-        elif (row1[0] == "O"):
-            player_1_wins = True
-    elif (row1[1] == row2[1] and row2[1] == row3[1] and (row1[1] != " " and row2[1] != " " and row3[1] != " ")):
-        if (row1[1] == "X"):
-            player_2_wins = True
-        elif (row1[1] == "O"):
-            player_1_wins = True
-    elif (row1[2] == row2[2] and row2[2] == row3[2] and (row1[2] != " " and row2[2] != " " and row3[2] != " ")):
-        if (row1[2] == "X"):
-            player_2_wins = True
-        elif (row1[2] == "O"):
-            player_1_wins = True
-    elif (row1[0] == row2[1] and row2[1] == row3[2] and (row1[0] != " " and row2[1] != " " and row3[2] != " ")):
-        if (row1[0] == "X"):
-            player_2_wins = True
-        elif (row1[0] == "O"):
-            player_1_wins = True
-    elif (row1[2] == row2[1] and row2[1] == row3[0] and (row1[2] != " " and row2[1] != " " and row3[0] != " ")):
-        if (row1[2] == "X"):
-            player_2_wins = True
-        elif (row1[2] == "O"):
-            player_1_wins = True
-
-    if player_1_wins:
-        return "player 1 wins"
-    elif player_2_wins:
-        return "player 2 wins"
-    else:
-        return "no one wins"
-
-
-def start_game():
-    print(prompt)
+def run_game():
     while True:
-        display(row1, row2, row3)
+        display()
         while True:
-            choice = user_choice()
-            if update_table(choice):
+            if update_table():
                 break
             else:
                 print("Wrong position to put your choice")
-                
-        result = check_winning()
-        if result == "player 1 wins":
-            display(row1, row2, row3)
-            print("Player 1 wins!! Congrats")
-            return
-        elif result == "player 2 wins":
-            display(row1, row2, row3)
-            print("Player 2 wins!! Congrats")
-            return
-        elif result == "no one wins" and counter == 9:
-            display(row1, row2, row3)
-            print("Tie game!!")
+        if win_or_lose():
             return
 
-
-start_game()
+run_game()
